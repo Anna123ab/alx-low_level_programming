d8d10999fd51a6617ea69f6955912a31b8caab60
Anna123ab <app1formgo112@gmail.com>
2023-03-24 04:33:02 -0700

    task 3 correct

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
 * main -  main entry point
 *hello c puts function
-*return : always 0(success)
+*Return: always 0(success)
 */
-int main (void)
+int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
