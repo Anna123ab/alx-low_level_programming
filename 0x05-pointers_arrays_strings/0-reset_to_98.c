9c1f49068bd774ea483430ae622b690f59f5c9b7
Anna123ab <app1formgo112@gmail.com>
2023-04-01 03:36:52 -0700

    task 0 correct

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* rest_to_98 - takes a pointer to an int parameter
+* reset_to_98 - takes a pointer to an int parameter
 *	and updates the value it points to 98
 *
 * @n: int parameter
@@ -11,5 +11,5 @@
 
 void reset_to_98(int *n)
 {
-	*n=98;
+	*n = 98;
 }
